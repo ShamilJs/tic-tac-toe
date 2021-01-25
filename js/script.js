f632de6e9c74c06bebb7c535213f103561a6b414
Shamil <Shamilllaa@mail.ru>
2021-01-26 04:29:10 +0700

    сделал комп и адаптив

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// открытие/закрытие меню настроек игры
 const openPopup = () => {
     const navBurger = document.querySelector('.nav_nav__burger');
     const popup = document.querySelector('.settings');
@@ -12,14 +13,38 @@ const openPopup = () => {
 openPopup();
 
 // -----------------------------------------------------алгоритм игры ---------------------------------------
-const element = document.querySelectorAll('.tic-tac__item');
-const alert = document.querySelector('.alert > p');
-const alertDiv = document.querySelector('.alert');
-const btnUpdate = document.querySelector('.alert__btn');
-let state = true;
-let content = '';
+const element = document.querySelectorAll('.tic-tac__item'),
+	alert = document.querySelector('.alert > p'),
+	alertDiv = document.querySelector('.alert'),
+	btnUpdate = document.querySelector('.alert__btn'),
+	tabMode = document.querySelectorAll('.tab__gamer'),
+	tabMove = document.querySelectorAll('.tab__move'),
+	tabBlock = document.querySelector('.tab');
 
+// Настройки игры
+const state = {
+	state: true,
+	content: '',
+	exception: [],
+	showWins: false,
+	mode: false,
+	move: false
+};
+
+// формирования контента для верстки
+const checkTextContent = () => {
+    if (state.state) state.content = '×';
+    else state.content = '○';
+};
+checkTextContent();
+
+// обновление игры
 const updateGame = () => {
+	state.exception = [];
+	state.showWins = false;
+	state.state = state.move ? false : true;
+	checkTextContent();
+
 	element.forEach(item => {
 		item.textContent = '';
 		if (item.matches('.tic-tac__active')) item.classList.remove('tic-tac__active');
@@ -27,63 +52,101 @@ const updateGame = () => {
 		alertDiv.style.display = 'none';
 		alertDiv.style.visibility = 'hidden';
 	});
-}
+};
 
-const checkTextContent = () => {
-    if (state) content = '×';
-    else content = '○';
-}
-checkTextContent();
+// Изменение режима игры
+const changeMode = () => {
+	updateGame();
+	state.mode = !state.mode;
+	tabMode.forEach(item => item.classList.toggle('gamer-active'));
+};
 
+// Изменение первого хода (с ноликов или крестиков)
+const changeMove = () => {
+	state.move = !state.move;
+	updateGame();
+	tabMove.forEach(item => item.classList.toggle('move-active'));
+};
+
+// Показать сообщение о статусе окончания игры
 const showAlert = (index = false, arr) => {
-	let text = ''
+	let text = '';
+	state.showWins = true;
 	if (typeof index === 'number') {
 		arr.forEach(item => item.classList.add('tic-tac-wins'));
 		if (element[index].textContent === '×') text = 'Победили крестики !!!';
 		else text = 'Победили нолики !!!';
-		
 	} else text = 'Победили дружба !!!';
-	
 	alert.textContent = text;
 	alertDiv.style.display = 'flex';
 	alertDiv.style.visibility = 'visible';
-}
+};
 
-const checkWinner = (index) => {
+// Выбор победителя
+const checkWinner = index => {
 	let count = 0;
-	let arr = []
-	if (element[0].textContent === element[1].textContent && element[0].textContent === element[2].textContent) arr = [element[0], element[1], element[2]];
-	else if (element[3].textContent === element[4].textContent && element[3].textContent === element[5].textContent) arr = [element[3], element[4], element[5]];
-	else if	(element[6].textContent === element[7].textContent && element[6].textContent === element[8].textContent) arr = [element[6], element[7], element[8]];
-	else if	(element[0].textContent === element[3].textContent && element[0].textContent === element[6].textContent) arr = [element[0], element[3], element[6]];
-	else if	(element[1].textContent === element[4].textContent && element[1].textContent === element[7].textContent) arr = [element[1], element[4], element[7]];
-	else if	(element[2].textContent === element[5].textContent && element[2].textContent === element[8].textContent) arr = [element[2], element[5], element[8]];
-	else if	(element[0].textContent === element[4].textContent && element[0].textContent === element[8].textContent) arr = [element[0], element[4], element[8]];
-	else if	(element[2].textContent === element[4].textContent && element[2].textContent === element[6].textContent) arr = [element[2], element[4], element[6]]; 
+	let arr = [];
+	let arr2 = [];
+	element.forEach(item => arr2.push(item.textContent));
+	if (arr2[0] === arr2[1] && arr2[0] === arr2[2] && arr2[0] !== '') arr = [element[0], element[1], element[2]];
+	if (arr2[3] === arr2[4] && arr2[3] === arr2[5] && arr2[3] !== '') arr = [element[3], element[4], element[5]];
+	if (arr2[6] === arr2[7] && arr2[6] === arr2[8] && arr2[6] !== '') arr = [element[6], element[7], element[8]];
+	if (arr2[0] === arr2[3] && arr2[0] === arr2[6] && arr2[0] !== '') arr = [element[0], element[3], element[6]];
+	if (arr2[1] === arr2[4] && arr2[1] === arr2[7] && arr2[1] !== '') arr = [element[1], element[4], element[7]];
+	if (arr2[2] === arr2[5] && arr2[2] === arr2[8] && arr2[2] !== '') arr = [element[2], element[5], element[8]];
+	if (arr2[0] === arr2[4] && arr2[0] === arr2[8] && arr2[0] !== '') arr = [element[0], element[4], element[8]];
+	if (arr2[2] === arr2[4] && arr2[2] === arr2[6] && arr2[2] !== '') arr = [element[2], element[4], element[6]]; 
 
 	element.forEach(item => {
-		if (item.textContent === '') return
+		if (item.textContent === '') return;
 		else count ++;
-	})
+	});
 	if (count === element.length) showAlert();
-	if (!arr.length || arr[0].textContent === '') return
+
+	if (!arr.length) return;
 	else {
-		element.forEach(item => item.classList.add('tic-tac__active'))
-		showAlert(index, arr)
+		element.forEach(item => item.classList.add('tic-tac__active'));
+		showAlert(index, arr);
 	}
 };
 
+// режим игры с компьютером
+const computerGames = exp => {
+	const min = 0,
+		max = 8;
+	while(true) {
+		let random = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+		if (exp.indexOf(random) < 0) return random;
+	}
+};
 
+// Сделать ход
+const makeAMove = index => {
+	element[index].textContent = state.content;
+    state.state = !state.state;
+	checkTextContent();
+	element[index].classList.add('tic-tac__active');
+	checkWinner(index);
+	state.exception.push(index);
+};
+
+// Обработчики событий
 element.forEach((item, i) => {
     item.addEventListener('click', () => {
-        item.textContent = content;
-        state = !state;
-		checkTextContent();
-		item.classList.add('tic-tac__active');
-		checkWinner(i)
-    });
+		makeAMove(i);
+		if (state.exception.length === element.length || state.showWins || !state.mode) return;
+		else {
+			const random = computerGames(state.exception);
+			makeAMove(random);
+		}
+	});
 });
 
-btnUpdate.addEventListener('click', () => updateGame());
+btnUpdate.addEventListener('click', updateGame);
+
+tabBlock.addEventListener('click', e => {
+	if (e.target.matches('.tab__gamer')) changeMode();
+	if (e.target.matches('.tab__move') || e.target.matches('.tab__move > p')) changeMove();
+});
 
 
